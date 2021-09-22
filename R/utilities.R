fc44a69e708b0b7c61619c26a4e2571385f3492b
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-09-22 15:40:53 +0200

    Converted indentation to spaces
    The rest of the package seems to be using spaces, and so does parts of that file.

--- R/utilities.R
+++ R/utilities.R
@@ -7,24 +7,24 @@ currentPackageName <- function(pkg) {
 }
 
 currentGitHubRef <- function(ref) {
-	if (!is.null(ref)) {return(ref)}
+  if (!is.null(ref)) {return(ref)}
 
-	url <- desc::desc_get_field("URL")
-	if (!grepl("gitHub", url, ignore.case = TRUE)) {
-		stop("reference could not be determined by the DESCRIPTION' url")
-	}
+  url <- desc::desc_get_field("URL")
+  if (!grepl("gitHub", url, ignore.case = TRUE)) {
+    stop("reference could not be determined by the DESCRIPTION' url")
+  }
 
-	url <- unlist(strsplit(url, ",\\s*"))
-	url <- url[grepl("github", url, ignore.case = TRUE)][[1]]
+  url <- unlist(strsplit(url, ",\\s*"))
+  url <- url[grepl("github", url, ignore.case = TRUE)][[1]]
 
-	ref <- gsub("https://github.com/", "", url)
+  ref <- gsub("https://github.com/", "", url)
 
-	# Removes eventual trailing slash on URL to fix svg filename
+  # Removes eventual trailing slash on URL to fix svg filename
   if (substring(ref, nchar(ref)) == "/") {
     ref <- gsub(".$", "", ref)
   }
 
-	ref
+  ref
 }
 
 defaultBranch <- function(branch) {
