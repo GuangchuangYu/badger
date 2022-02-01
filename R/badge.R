d0d24caf6dd1d68c747deb5f95e52d6f2b02a02d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-02-01 08:34:09 +0100

    Replaced unicode characters
    This fixes the remote CI and R-hub checks.

--- R/badge.R
+++ R/badge.R
@@ -250,14 +250,14 @@ badge_repostatus <- function(status, alt = NULL) {
   if (is.null(alt)) {
     alt <- switch(
       status,
-      concept     = "Concept – Minimal or no implementation has been done yet, or the repository is only intended to be a limited example, demo, or proof-of-concept.",
-      wip         = "WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.",
-      suspended   = "Suspended – Initial development has started, but there has not yet been a stable, usable release; work has been stopped for the time being but the author(s) intend on resuming work.",
-      abandoned   = "Abandoned – Initial development has started, but there has not yet been a stable, usable release; the project has been abandoned and the author(s) do not intend on continuing development.",
-      active      = "Active – The project has reached a stable, usable state and is being actively developed.",
-      inactive    = "Inactive – The project has reached a stable, usable state but is no longer being actively developed; support/maintenance will be provided as time allows.",
-      unsupported = "Unsupported – The project has reached a stable, usable state but the author(s) have ceased all work on it. A new maintainer may be desired.",
-      moved       = "Moved to http://example.com – The project has been moved to a new location, and the version at that location should be considered authoritative.",
+      concept     = "Concept - Minimal or no implementation has been done yet, or the repository is only intended to be a limited example, demo, or proof-of-concept.",
+      wip         = "WIP - Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.",
+      suspended   = "Suspended - Initial development has started, but there has not yet been a stable, usable release; work has been stopped for the time being but the author(s) intend on resuming work.",
+      abandoned   = "Abandoned - Initial development has started, but there has not yet been a stable, usable release; the project has been abandoned and the author(s) do not intend on continuing development.",
+      active      = "Active - The project has reached a stable, usable state and is being actively developed.",
+      inactive    = "Inactive - The project has reached a stable, usable state but is no longer being actively developed; support/maintenance will be provided as time allows.",
+      unsupported = "Unsupported - The project has reached a stable, usable state but the author(s) have ceased all work on it. A new maintainer may be desired.",
+      moved       = "Moved to http://example.com - The project has been moved to a new location, and the version at that location should be considered authoritative.",
       stop("invalid status: ", status)
     )
     alt <- paste("Project Status:", alt)
